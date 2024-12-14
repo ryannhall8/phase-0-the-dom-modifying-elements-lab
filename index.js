bcedcf379e4e7c45442a28a8011629adea9c69cc
Ryann <ryann.hall2004@gmail.com>
2024-12-14 00:27:09 -0500

    finshed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Ryann is the champion'; 
+
+document.body.appendChild(newHeader);
